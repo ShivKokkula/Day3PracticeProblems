2b768447313942162b0dd7a5eb6d053a82df2dbc
ShivKokkula <shivbk987@gmail.com>
2021-05-22 17:42:35 +0530

    Refactor - added calculate lenght function in the program

--- LineComparison.java
+++ LineComparison.java
@@ -5,7 +5,15 @@ public class LineComparison {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Line Comparison Computation Program");
+		
+		double result = calLenght(0.0,0.0,4.0,1.0);
+		System.out.println(result);
 
+	}
+	
+	public static double calLenght(double x1, double y1, double x2, double y2) {
+		 double lenght = Math.sqrt(Math.pow((x2 - x1),2) - Math.pow((y2 - y1),2));
+		 return lenght;	
 	}
 
 }
