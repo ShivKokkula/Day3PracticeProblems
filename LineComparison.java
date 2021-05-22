fe7ec7499e78ad3eeb4fba06891a3ca6189eebc7
ShivKokkula <shivbk987@gmail.com>
2021-05-22 18:09:21 +0530

    Refactor- added check equality, greater or smaller feature in the program

--- LineComparison.java
+++ LineComparison.java
@@ -6,11 +6,11 @@ public class LineComparison {
 
 		System.out.println("Welcome to Line Comparison Computation Program");
 		
-		double line1 = calLenght(0.0,0.0,4.0,1.0);
-		double line2 = calLenght(0.0,0.0,4.0,1.0);
+		double line1 = calLenght(0.0,0.0,8.0,1.0);
+		double line2 = calLenght(0.0,0.0,10.0,1.0);
 		
-		System.out.println(line1);
-		System.out.println(line2);
+		System.out.println("Lenght of Line1: "+line1);
+		System.out.println("Lenght of Line2: "+line2);
 		
 		checkLineLenght(line1,line2);
 
@@ -23,8 +23,12 @@ public class LineComparison {
 	
 	public static void checkLineLenght(double line1,double line2) {
 		
-		if(line1 == line2) {
+		if(line1 > line2) {
+			System.out.println("Line1 is greater than Line2");
+		}else if(line1 == line2){
 			System.out.println("Two line of are equal lenght");
+		}else {
+			System.out.println("Line1 is smaller than Line");
 		}
 	}
 
